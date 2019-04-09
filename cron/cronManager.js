6604af454ebcf1dbe7c139d09e818ccd5193af3b
waheng chang <wahengchang@gmail.com>
2019-04-09 08:35:54 +0800

    added cron log

--- cron/cronManager.js
+++ cron/cronManager.js
@@ -4,8 +4,12 @@ var cmd = require('@pscraper/cmd');
 
 console.log('[INFO] cron job is started ...')
 
+const AUTH_V2_CRON = '00 07 * * *'
+
+console.log('[INFO] auth cron: ', AUTH_V2_CRON)
+
 // run provider every 11:00
-cron.schedule('00 13 * * *', function(){
+cron.schedule(AUTH_V2_CRON, function(){
   cmd("node ./main/index").then(() => {
     console.log('[INFO] auth v2 login done')
   })
