275a719ceafbfe9a4487319841cb627e08c090cf
waheng chang <wahengchang@gmail.com>
2019-05-22 10:13:29 +0800

    update cron manager

--- cron/cronManager.js
+++ cron/cronManager.js
@@ -4,12 +4,19 @@ var cmd = require('@pscraper/cmd');
 
 console.log('[INFO] cron job is started ...')
 
-const AUTH_V2_CRON = '00 07 * * *'
+const AUTH_V2_CRON_1 = '00 07 * * *'
+const AUTH_V2_CRON_2 = '00 19 * * *'
 
-console.log('[INFO] auth cron: ', AUTH_V2_CRON)
+console.log('[INFO] auth cron: ', AUTH_V2_CRON_1)
+console.log('[INFO] auth cron: ', AUTH_V2_CRON_2)
 
-// run provider every 11:00
-cron.schedule(AUTH_V2_CRON, function(){
+cron.schedule(AUTH_V2_CRON_1, function(){
+  cmd("node ./main/index").then(() => {
+    console.log('[INFO] auth v2 login done')
+  })
+});
+
+cron.schedule(AUTH_V2_CRON_2, function(){
   cmd("node ./main/index").then(() => {
     console.log('[INFO] auth v2 login done')
   })
