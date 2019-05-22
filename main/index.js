b52efe615dc80125e94f139265cfcfe3077e1db3
waheng chang <wahengchang@gmail.com>
2019-05-22 10:13:17 +0800

    added continuous error count

--- main/index.js
+++ main/index.js
@@ -1,9 +1,11 @@
 (async function(){
   const keys = require('../keys.json')
   const authDao = require('../db/auth/dao')
+  const {printCurrentTime} = require('../lib/time')
   const shell = require('./childHelper')
   const extractCookie = require('./extractCookie')
   const extractEnuid = require('./extractEnuid')
+  const MAX_CONTINUOUS_ERROR = 10
 
   const withinNHour = (targetDate, n = 10) => {
     const ONE_HOUR = 60 * 60 * 1000; /* ms */
@@ -21,16 +23,23 @@
   }
 
   const {count, data} = await authDao.findAndCountAll()
-  console.log('[INFO] auth obj total    :', count)
-
   const unavailableList = data.filter(unavailableCondition)
+  printCurrentTime()
+  console.log('[INFO] auth obj total    :', count)
   console.log('[INFO] unavailable total :', unavailableList.length)
 
+  let continuousErrorCount = 0
   for(let i =0 ; i<unavailableList.length; i++) {
+    if(continuousErrorCount >= MAX_CONTINUOUS_ERROR) {
+      console.log('[ERROR] continuousErrorCount >= MAX_CONTINUOUS_ERROR for loop:')
+      continuousErrorCount = 0
+      continue
+    }
+    
     const item = unavailableList[i]
     const {username} = item
     const password = keys[username]
-    
+
     try {
       console.log('username: ', username)
       console.log('password: ', password)
@@ -46,15 +55,17 @@
       const body = {...cookies, ...enuid, status, statusNat}
       console.log(body)
       await authDao.update(body, {username})
+      continuousErrorCount =0 
       console.log('[INFO] update DB success')
     }
     catch(err){
       console.log('[ERROR] main/index for loop:', err)
       // throw ''
+      continuousErrorCount+=1
       i-=1
       continue
     }
   }
-  
+  printCurrentTime()
   process.exit()
 })()
